8085120be4856462e53cffdf632497848b9b7bf3
talmeym <mtalmey@gmail.com>
2016-01-06 15:03:38 +0000

    renamed http-call-through as http-call

--- src/test/java/com/emarte/regurgitator/test/HttpCallXmlLoaderTest.java
+++ src/test/java/com/emarte/regurgitator/test/HttpCallXmlLoaderTest.java
@@ -1,7 +1,7 @@
 package com.emarte.regurgitator.test;
 
 import com.emarte.regurgitator.core.*;
-import com.emarte.regurgitator.extensions.web.HttpCallThroughXmlLoader;
+import com.emarte.regurgitator.extensions.web.HttpCallXmlLoader;
 import org.dom4j.DocumentException;
 import org.junit.Test;
 import org.xml.sax.SAXException;
@@ -11,17 +11,17 @@ import java.util.HashSet;
 
 import static junit.framework.Assert.assertEquals;
 
-public class HttpCallThroughXmlLoaderTest extends XmlBaseTest {
-	private HttpCallThroughXmlLoader toTest = new HttpCallThroughXmlLoader();
+public class HttpCallXmlLoaderTest extends XmlBaseTest {
+	private HttpCallXmlLoader toTest = new HttpCallXmlLoader();
 
 	@Test
 	public void testMin() throws DocumentException, SAXException, IOException, RegurgitatorException {
-		assertExpectation("classpath:/HttpCallThrough_min.xml", "com.emarte.regurgitator.extensions.web.HttpCallThrough:['http-call-through-1',com.emarte.regurgitator.extensions.web.HttpMessageProxy:['something.com',1234],null]");
+		assertExpectation("classpath:/HttpCall_min.xml", "com.emarte.regurgitator.extensions.web.HttpCall:['http-call-1',com.emarte.regurgitator.extensions.web.HttpMessageProxy:['something.com',1234],null]");
 	}
 
 	@Test
 	public void testFullLoad() throws DocumentException, SAXException, IOException, RegurgitatorException {
-		ConfigurationFile.loadFile("classpath:/HttpCallThrough_min.xml");
+		ConfigurationFile.loadFile("classpath:/HttpCall_min.xml");
 	}
 
 	private void assertExpectation(String filePath, String expected) throws RegurgitatorException, SAXException, DocumentException, IOException {
