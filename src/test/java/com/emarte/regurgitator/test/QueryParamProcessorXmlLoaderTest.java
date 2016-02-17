a833d5851c1a05faab700d5ae4b278887375ba82
talmeym <mtalmey@gmail.com>
2016-02-17 14:54:03 +0000

    added fullLoad test for missing xml loaders

--- src/test/java/com/emarte/regurgitator/test/QueryParamProcessorXmlLoaderTest.java
+++ src/test/java/com/emarte/regurgitator/test/QueryParamProcessorXmlLoaderTest.java
@@ -1,7 +1,10 @@
 package com.emarte.regurgitator.test;
 
+import com.emarte.regurgitator.core.*;
 import com.emarte.regurgitator.extensions.web.QueryParamProcessorXmlLoader;
 import org.junit.Test;
+
+import static com.emarte.regurgitator.core.ConfigurationFile.loadFile;
 
 public class QueryParamProcessorXmlLoaderTest extends XmlLoaderTest {
 	public QueryParamProcessorXmlLoaderTest() {
@@ -11,5 +14,10 @@ public class QueryParamProcessorXmlLoaderTest extends XmlLoaderTest {
 	@Test
 	public void testThis() throws Exception {
 		assertExpectation("classpath:/QueryParamProcessor.xml", "com.emarte.regurgitator.extensions.web.QueryParamProcessor:['value']");
+	}
+
+	@Test
+	public void testFullLoad() throws RegurgitatorException {
+		loadFile("classpath:/QueryParamProcessor_fullLoad.xml");
 	}
 }
