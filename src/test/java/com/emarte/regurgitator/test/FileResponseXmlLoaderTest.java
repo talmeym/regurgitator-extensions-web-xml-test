62373baee7249eabdc59c018f52dc346407eb5f3
talmeym <mtalmey@gmail.com>
2017-02-15 12:14:12 +0000

    Changed to use jvm supplied xml / xpath processing

--- src/test/java/com/emarte/regurgitator/test/FileResponseXmlLoaderTest.java
+++ src/test/java/com/emarte/regurgitator/test/FileResponseXmlLoaderTest.java
@@ -1,12 +1,7 @@
 package com.emarte.regurgitator.test;
 
-import com.emarte.regurgitator.core.RegurgitatorException;
 import com.emarte.regurgitator.extensions.web.FileResponseXmlLoader;
-import org.dom4j.DocumentException;
 import org.junit.Test;
-import org.xml.sax.SAXException;
-
-import java.io.IOException;
 
 public class FileResponseXmlLoaderTest extends XmlLoaderTest {
 	public FileResponseXmlLoaderTest() {
@@ -14,12 +9,12 @@ public class FileResponseXmlLoaderTest extends XmlLoaderTest {
 	}
 
 	@Test
-	public void testThis() throws SAXException, IOException, DocumentException, RegurgitatorException {
+	public void testThis() throws Exception {
 		assertExpectation("classpath:/FileResponse.xml", "com.emarte.regurgitator.extensions.web.FileResponse:['file-response-1',com.emarte.regurgitator.core.ValueSource:[com.emarte.regurgitator.core.ContextLocation:['request-metadata:path-info'],null],null]");
 	}
 
 	@Test
-	public void testPrefixed() throws SAXException, IOException, DocumentException, RegurgitatorException {
+	public void testPrefixed() throws Exception {
 		assertExpectation("classpath:/FileResponse_prefixed.xml", "com.emarte.regurgitator.extensions.web.FileResponse:['file-response-1',com.emarte.regurgitator.core.ValueSource:[com.emarte.regurgitator.core.ContextLocation:['request-metadata:path-info'],null],'/assets']");
 	}
 }
--- src/test/java/com/emarte/regurgitator/test/HttpCallXmlLoaderTest.java
+++ src/test/java/com/emarte/regurgitator/test/HttpCallXmlLoaderTest.java
@@ -2,11 +2,7 @@ package com.emarte.regurgitator.test;
 
 import com.emarte.regurgitator.core.RegurgitatorException;
 import com.emarte.regurgitator.extensions.web.HttpCallXmlLoader;
-import org.dom4j.DocumentException;
 import org.junit.Test;
-import org.xml.sax.SAXException;
-
-import java.io.IOException;
 
 import static com.emarte.regurgitator.core.ConfigurationFile.loadFile;
 
@@ -16,27 +12,27 @@ public class HttpCallXmlLoaderTest extends XmlLoaderTest {
 	}
 
 	@Test
-	public void testMin() throws DocumentException, SAXException, IOException, RegurgitatorException {
+	public void testMin() throws Exception {
 		assertExpectation("classpath:/HttpCall_min.xml", "com.emarte.regurgitator.extensions.web.HttpCall:['http-call-1',com.emarte.regurgitator.extensions.web.HttpMessageProxy:[com.emarte.regurgitator.extensions.web.HttpClientWrapper:['something.com',1234,null,null]],[]]");
 	}
 
 	@Test
-	public void testMax() throws DocumentException, SAXException, IOException, RegurgitatorException {
+	public void testMax() throws Exception {
 		assertExpectation("classpath:/HttpCall_max.xml", "com.emarte.regurgitator.extensions.web.HttpCall:['http-call-1',com.emarte.regurgitator.extensions.web.HttpMessageProxy:[com.emarte.regurgitator.extensions.web.HttpClientWrapper:['something.com',1234,'username','password']],[com.emarte.regurgitator.test.stuff.TestStep:['test-step-1'], com.emarte.regurgitator.test.stuff.TestStep:['test-step-2']]]");
 	}
 
 	@Test(expected = RegurgitatorException.class)
-	public void testMissingUsername() throws DocumentException, SAXException, IOException, RegurgitatorException {
+	public void testMissingUsername() throws Exception {
 		loadFromFile("classpath:/HttpCall_missingUsername.xml");
 	}
 
 	@Test(expected = RegurgitatorException.class)
-	public void testMissingPassword() throws DocumentException, SAXException, IOException, RegurgitatorException {
+	public void testMissingPassword() throws Exception {
 		loadFromFile("classpath:/HttpCall_missingPassword.xml");
 	}
 
 	@Test
-	public void testFullLoad() throws DocumentException, SAXException, IOException, RegurgitatorException {
+	public void testFullLoad() throws Exception {
 		loadFile("classpath:/HttpCall_min.xml");
 	}
 }
