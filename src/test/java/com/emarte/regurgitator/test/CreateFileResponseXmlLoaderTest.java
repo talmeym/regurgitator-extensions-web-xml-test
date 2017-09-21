03290f7f6f150f14373873fb706ce5f96af98cc7
talmeym <mtalmey@gmail.com>
2017-09-21 11:12:32 +0100

    renamed FileResponse CreateFileResponse

---
+++ src/test/java/com/emarte/regurgitator/test/CreateFileResponseXmlLoaderTest.java
@@ -0,0 +1,28 @@
+package com.emarte.regurgitator.test;
+
+import com.emarte.regurgitator.core.RegurgitatorException;
+import com.emarte.regurgitator.extensions.web.CreateFileResponseXmlLoader;
+import org.junit.Test;
+
+import static com.emarte.regurgitator.core.ConfigurationFile.loadFile;
+
+public class CreateFileResponseXmlLoaderTest extends XmlLoaderTest {
+	public CreateFileResponseXmlLoaderTest() {
+		super(new CreateFileResponseXmlLoader());
+	}
+
+	@Test
+	public void testThis() throws Exception {
+		assertExpectation("classpath:/CreateFileResponse.xml", "com.emarte.regurgitator.extensions.web.CreateFileResponse:['create-file-response-1',com.emarte.regurgitator.core.ValueSource:[com.emarte.regurgitator.core.ContextLocation:['request-metadata:path-info'],null],null]");
+	}
+
+	@Test
+	public void testPrefixed() throws Exception {
+		assertExpectation("classpath:/CreateFileResponse_prefixed.xml", "com.emarte.regurgitator.extensions.web.CreateFileResponse:['create-file-response-1',com.emarte.regurgitator.core.ValueSource:[com.emarte.regurgitator.core.ContextLocation:['request-metadata:path-info'],null],'/assets']");
+	}
+
+	@Test
+	public void testFullLoadXml() throws RegurgitatorException {
+		loadFile("classpath:/CreateFileResponse_fullLoad.xml");
+	}
+}
